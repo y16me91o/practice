0a716dd7b463860618a41664c655bb8f0b4d5b4b
shaikabd <shaikabdul4217@gmail.com>
2023-08-27 19:59:46 +0530

    commiting helloworld project 1st time

---
+++ src/main/java/com/infy/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.infy.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
