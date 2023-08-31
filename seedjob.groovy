7f24b46a4e63720ac61046a9bd532f8b60cff564
y16me91o <140883640+y16me91o@users.noreply.github.com>
2023-08-31 09:23:53 +0530

    added seed file from github

---
+++ seedjob.groovy
@@ -0,0 +1,14 @@
+job("Helloworld-war via seed DSL") {
+    description "Builds helloworld-war job on ${new Date()} via seed DSL"
+    scm {
+        git("https://github.com/y16me91o/practice.git",helloworld-war-seed)
+        
+    }
+    steps {
+        shell "Look: I'm building helloworld-war-seed!"
+        maveen('clean package')
+    }
+    publishers{
+        archiveArtifacts '**/*.war'
+    }
+}
